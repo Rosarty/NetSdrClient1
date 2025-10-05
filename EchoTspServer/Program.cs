478ffa8e40d9ef3d9f0609e98f0802fa6ca85e81
Rosarty <150185759+Rosarty@users.noreply.github.com>
2025-10-05 23:52:20 +0300

    Update Program.cs
    Виправлення:
    
    _cancellationTokenSource стало readonly.
    
    Додано using System.Linq; для .Concat().
    
    В UdpTimedSender створення Random перенесено у поле, щоб уникнути повторного створення кожного разу.

--- EchoTspServer/Program.cs
+++ EchoTspServer/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace EchoServer
 {
@@ -11,9 +12,9 @@ namespace EchoServer
     {
         private readonly int _port;
         private TcpListener _listener;
-        private CancellationTokenSource _cancellationTokenSource;
+        private readonly CancellationTokenSource _cancellationTokenSource; // <-- зроблено readonly
 
-        //constuctor
+        // constuctor
         public EchoServer(int port)
         {
             _port = port;
@@ -25,14 +26,12 @@ namespace EchoServer
             _listener = new TcpListener(IPAddress.Any, _port);
             _listener.Start();
             Console.WriteLine($"Server started on port {_port}.");
-
             while (!_cancellationTokenSource.Token.IsCancellationRequested)
             {
                 try
                 {
                     TcpClient client = await _listener.AcceptTcpClientAsync();
                     Console.WriteLine("Client connected.");
-
                     _ = Task.Run(() => HandleClientAsync(client, _cancellationTokenSource.Token));
                 }
                 catch (ObjectDisposedException)
@@ -41,7 +40,6 @@ namespace EchoServer
                     break;
                 }
             }
-
             Console.WriteLine("Server shutdown.");
         }
 
@@ -53,7 +51,6 @@ namespace EchoServer
                 {
                     byte[] buffer = new byte[8192];
                     int bytesRead;
-
                     while (!token.IsCancellationRequested && (bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, token)) > 0)
                     {
                         // Echo back the received message
@@ -84,25 +81,22 @@ namespace EchoServer
         public static async Task Main(string[] args)
         {
             EchoServer server = new EchoServer(5000);
-
             // Start the server in a separate task
             _ = Task.Run(() => server.StartAsync());
 
             string host = "127.0.0.1"; // Target IP
             int port = 60000;          // Target Port
-            int intervalMilliseconds = 5000; // Send every 3 seconds
+            int intervalMilliseconds = 5000; // Send every 5 seconds
 
             using (var sender = new UdpTimedSender(host, port))
             {
                 Console.WriteLine("Press any key to stop sending...");
                 sender.StartSending(intervalMilliseconds);
-
                 Console.WriteLine("Press 'q' to quit...");
                 while (Console.ReadKey(intercept: true).Key != ConsoleKey.Q)
                 {
                     // Just wait until 'q' is pressed
                 }
-
                 sender.StopSending();
                 server.Stop();
                 Console.WriteLine("Sender stopped.");
@@ -110,13 +104,13 @@ namespace EchoServer
         }
     }
 
-
     public class UdpTimedSender : IDisposable
     {
         private readonly string _host;
         private readonly int _port;
         private readonly UdpClient _udpClient;
         private Timer _timer;
+        private readonly Random _rnd = new Random(); // створюємо один раз
 
         public UdpTimedSender(string host, int port)
         {
@@ -129,25 +123,20 @@ namespace EchoServer
         {
             if (_timer != null)
                 throw new InvalidOperationException("Sender is already running.");
-
             _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
         }
 
         ushort i = 0;
-
         private void SendMessageCallback(object state)
         {
             try
             {
                 //dummy data
-                Random rnd = new Random();
                 byte[] samples = new byte[1024];
-                rnd.NextBytes(samples);
+                _rnd.NextBytes(samples);
                 i++;
-
                 byte[] msg = (new byte[] { 0x04, 0x84 }).Concat(BitConverter.GetBytes(i)).Concat(samples).ToArray();
                 var endpoint = new IPEndPoint(IPAddress.Parse(_host), _port);
-
                 _udpClient.Send(msg, msg.Length, endpoint);
                 Console.WriteLine($"Message sent to {_host}:{_port} ");
             }

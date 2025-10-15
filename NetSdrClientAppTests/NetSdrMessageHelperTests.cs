c07f5fdf1a0b4dc77169397c110df0207d4cabbc
Ростислав <ros.artym@gmail.com>
2025-10-15 14:07:47 +0300

    4 тести апдейт

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -66,7 +66,34 @@ namespace NetSdrClientAppTests
 
             Assert.That(msg.Length, Is.GreaterThanOrEqualTo(4)); // тільки заголовок + код
         }
+        Test]
+public void GetControlItemMessage_ShouldEncodeAndDecodeProperly()
+        {
+            var type = NetSdrMessageHelper.MsgTypes.Ack;
+            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
 
+            // Вхідні дані (у параметрах передаємо байти з відомим вмістом)
+            byte[] parameters = Enumerable.Range(0, 10).Select(i => (byte)i).ToArray();
+
+            // Виклик методу
+            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, parameters);
+
+            // Розбір повідомлення
+            ushort num = BitConverter.ToUInt16(msg, 0);
+            var decodedType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
+            var decodedLength = num - ((int)decodedType << 13);
+            var decodedCode = BitConverter.ToInt16(msg, 2);
+            var decodedParameters = msg.Skip(4).ToArray();
+
+            // Перевірки
+            Assert.Multiple(() =>
+            {
+                Assert.That(decodedType, Is.EqualTo(type), "Тип повідомлення не збігається");
+                Assert.That(decodedCode, Is.EqualTo((short)code), "Код повідомлення не збігається");
+                Assert.That(decodedLength, Is.EqualTo(msg.Length), "Довжина повідомлення некоректна");
+                Assert.That(decodedParameters, Is.EqualTo(parameters), "Параметри повідомлення некоректно передані");
+            });
+        }
         // ✅ Новий тест 3 — Інший тип повідомлення для DataItem
         [Test]
         public void GetDataItemMessage_OtherType_WorksCorrectly()

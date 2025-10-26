cfe6a90132f3945bdc4be2e932a5129972669183
Ростислав <ros.artym@gmail.com>
2025-10-26 15:53:04 +0200

    update code

--- NetSdrClientAppTests/ArchitectureTests.cs
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -1,30 +1,54 @@
 ﻿using NetArchTest.Rules;
-using Xunit;
+using NUnit.Framework;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace NetSdrClientAppTests
 {
     public class ArchitectureTests
     {
-        [Fact]
-        public void NetSdrClientApp_Should_Not_Depend_On_EchoTspServer()
+        [Test]
+        public void App_Should_Not_Depend_On_EchoServer()
         {
-            var result = Types
-                .InAssembly(typeof(NetSdrClientApp.Program).Assembly)
+            var result = Types.InAssembly(typeof(NetSdrClientApp.NetSdrClient).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp")
                 .ShouldNot()
-                .HaveDependencyOn("EchoTspServer")
+                .HaveDependencyOn("EchoServer")
                 .GetResult();
 
+            Assert.That(result.IsSuccessful, Is.True);
         }
 
-        [Fact]
-        public void EchoTspServer_Should_Not_Depend_On_NetSdrClientApp()
+        [Test]
+        public void Messages_Should_Not_Depend_On_Networking()
         {
-            var result = Types
-                .InAssembly(typeof(EchoTspServer.Program).Assembly)
+            // Arrange
+            var result = Types.InAssembly(typeof(NetSdrClientApp.Messages.NetSdrMessageHelper).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Messages")
                 .ShouldNot()
-                .HaveDependencyOn("NetSdrClientApp")
+                .HaveDependencyOn("NetSdrClientApp.Networking")
                 .GetResult();
 
+            // Assert
+            Assert.That(result.IsSuccessful, Is.True);
+        }
+
+        [Test]
+        public void Networking_Should_Not_Depend_On_Messages()
+        {
+            // Arrange
+            var result = Types.InAssembly(typeof(NetSdrClientApp.Networking.ITcpClient).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Networking")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Messages")
+                .GetResult();
+
+            // Assert
+            Assert.That(result.IsSuccessful, Is.True);
         }
     }
 }

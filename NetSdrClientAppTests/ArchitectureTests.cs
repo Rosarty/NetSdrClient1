843c8d0c354d42fd7af4d2cf39f44b593c1d9847
Ростислав <ros.artym@gmail.com>
2025-10-26 15:05:28 +0200

    Додав файл до тестів

---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,32 @@
+﻿using NetArchTest.Rules;
+using Xunit;
+
+namespace NetSdrClientAppTests
+{
+	public class ArchitectureTests
+	{
+		[Fact]
+		public void NetSdrClientApp_Should_Not_Depend_On_EchoTspServer()
+		{
+			var result = Types
+				.InAssembly(typeof(NetSdrClientApp.Program).Assembly)
+				.ShouldNot()
+				.HaveDependencyOn("EchoTspServer")
+				.GetResult();
+
+			Assert.True(result.IsSuccessful, "❌ NetSdrClientApp має залежність від EchoTspServer!");
+		}
+
+		[Fact]
+		public void EchoTspServer_Should_Not_Depend_On_NetSdrClientApp()
+		{
+			var result = Types
+				.InAssembly(typeof(EchoTspServer.Program).Assembly)
+				.ShouldNot()
+				.HaveDependencyOn("NetSdrClientApp")
+				.GetResult();
+
+			Assert.True(result.IsSuccessful, "❌ EchoTspServer не повинен залежати від NetSdrClientApp!");
+		}
+	}
+}
